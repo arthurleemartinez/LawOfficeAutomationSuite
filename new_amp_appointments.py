cc7ff157107ed8207c12c2e6b19462bf8ab6f4d5
Arthur Martinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-11-30 15:36:02 -0600

    Create new_amp_appointments.py

---
+++ new_amp_appointments.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+import time
+import urllib3
+import selenium
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import requests
+from selenium.webdriver.common.keys import Keys
+import webbrowser
+import keyboard
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from _datetime import datetime
+
+def open_amp():
+    driver = webdriver.Chrome(executable_path='C:/Users/Arthur Martinez/chromedriver.exe')
+    driver.get('https://idc.traviscountytx.gov/nidp/idff/sso?id=35&sid=2&option=credential&sid=2&target=https%3A%2F%2Fcourts.traviscountytx.gov%2Famp%2F')
+    user1_XPATH = "//*[@id=\"main_content\"]/div[2]/form/input[1]"
+    pass1_XPATH = "//*[@id=\"main_content\"]/div[2]/form/input[2]"
+    access_server_XPATH = "//*[@id=\"main_content\"]/div[2]/form/div/p/input"
+    # find the elements to click and interact with
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.XPATH, user1_XPATH)))
+    user1 = driver.find_element_by_xpath(user1_XPATH)
+    wait.until(EC.presence_of_element_located((By.XPATH, pass1_XPATH)))
+    pass1 = driver.find_element_by_xpath(pass1_XPATH)
+    wait.until(EC.presence_of_element_located((By.XPATH, access_server_XPATH)))
+    access1 = driver.find_element_by_xpath(access_server_XPATH)
+    user1.send_keys("Sbn13142750")
+    pass1.send_keys("Lm13142750*")
+    access1.click()
+    driver.get("https://courts.traviscountytx.gov/AMP/Cases/Search")
+    xpath_startdate = "//*[@id=\"start\"]"
+    wait.until(EC.presence_of_element_located((By.XPATH, xpath_startdate)))
+    sd = driver.find_element_by_xpath(xpath_startdate)
+    sd.clear()
+    # date logic
+    dt = datetime.now()
+    tmrw_day_int = int(dt.day)
+    tmrw_day = str(tmrw_day_int)
+    tmrw_month = str(dt.month)
+    tmrw_yr = str(dt.year)
+    ds = "/"
+    entered_date = tmrw_month + ds + tmrw_day + ds + tmrw_yr
+    wait.until(EC.presence_of_element_located((By.XPATH, xpath_startdate)))
+    sd.send_keys(entered_date)
+    xpath_search4 = "//*[@id=\"case-search\"]/div[3]/div[2]/ul/li[1]/button"
+    wait.until(EC.presence_of_element_located((By.XPATH, xpath_search4)))
+    search4 = driver.find_element_by_xpath(xpath_search4)
+    search4.click()
+    time.sleep(15*60)
+    driver.close()
+
+open_amp()
